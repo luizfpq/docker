484108990879a85e23e38ad9ea729ad6e0760023
Luiz Quirino <luizfpq@gmail.com>
2021-08-17 12:33:44 -0300

    detach pgadmin / postgres

--- portainer/portainer.sh
+++ portainer/portainer.sh
@@ -1,1 +1,3 @@
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+!#/usr/bin/env bash
+
+docker run -d -p 9090:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /media/vault/docker_data/portainer_data:/data portainer/portainer-ce
